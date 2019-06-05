9513c22392e6156d49fee97ea37ca8560e49f29c
acrylic-style <me@acrylicstyle.xyz>
2019-06-05 21:57:23 +0900

    fix circular dependency

--- commands/reason.js
+++ commands/reason.js
@@ -1,6 +1,5 @@
 const cases = require('../data/cases.json')
 const { Command } = require('bot-framework')
-const { commands } = require('bot-framework/commands')
 
 module.exports = class extends Command {
   constructor() {
@@ -8,7 +7,7 @@ module.exports = class extends Command {
   }
 
   async run(msg, lang, args, sendDeletable) {
-    if (!args[2]) return commands['help'].start({ ...msg, content: 'help' }, lang, ['help', 'reason'])
+    if (!args[2]) return msg.channel.send('引数を指定してください。(<<該当するメッセージID> <理由>>)')
     if (!Object.keys(cases).includes(args[1])) return msg.channel.send('引数が正しくありません。')
     cases[args[1]].reason = args.slice(2).join(' ')
     sendDeletable(':white_check_mark: reasonを設定しました')
